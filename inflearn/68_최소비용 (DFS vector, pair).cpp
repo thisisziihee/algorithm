40bf0455ea4d0c21f4043363b2d67e9802ddd31c
thisisziihee <44192641+thisisziihee@users.noreply.github.com>
2020-02-27 15:30:30 +0900

    DFS 인접리스트

---
+++ inflearn/68_최소비용 (DFS vector, pair).cpp
@@ -0,0 +1,51 @@
+// 가중치 방향그래프가 주어지면 1번 정점에서 N번 정점으로 가는 최소비용을 출력하는 프로그램을 작성하세요.
+
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int n, minn = 2147000000;
+vector<pair <int, int> > map[21];
+int check[21];
+
+void dfs(int v, int sum) {
+	if (v == n) {
+		minn = (sum < minn ? sum : minn);
+	}
+	else {
+		for (int i = 0; i < map[v].size(); i++) {
+			if (check[map[v][i].first] == 0) {
+
+				check[map[v][i].first] = 1;
+				dfs(map[v][i].first, sum + map[v][i].second);
+				check[map[v][i].first] = 0;
+			}
+		}
+	}
+}
+
+int main() {
+	int m, i, start, end, val;
+	scanf("%d %d", &n, &m);
+	for (i = 0; i < m; i++) {
+		scanf("%d %d %d", &start, &end, &val);
+		map[start].push_back(make_pair(end, val));
+	}
+	check[1] = 1;
+	dfs(1, 0);
+	printf("%d", minn);
+}
+
+/*
+5 8
+1 2 12
+1 3 6
+1 4 10
+2 3 2
+2 5 2
+3 4 3
+4 2 2
+4 5 5
+
+*/
