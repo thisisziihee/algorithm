cd1e8a49e4f88bfdf6f0f65db6e5659c47466583
thisisziihee <44192641+thisisziihee@users.noreply.github.com>
2020-03-11 14:08:51 +0900

    DP, Bottom-Up

---
+++ inflearn/91_네트워크 선 자르기(DP, Bottom-Up).cpp
@@ -0,0 +1,30 @@
+/*
+현수는 네트워크 선을 1m, 2m 의 길이를 갖는 선으로 자르려고 합니다.
+예를 들어 4m의 네트워크 선이 주어진다면
+1m + 1m + 1m + 1m
+2m + 1m + 1m
+1m + 2m + 1m
+1m + 1m + 2m
+2m + 2m
+의 다섯가지 방법을 생각할 수 있습니다. 자르는 위치가 다르면 다른 경우로 본다.
+그렇다면 네트워크 선의 길이가 Nm라면 몇 가지의 자르는 방법을 생각할 수 있나요?
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int dy[50];
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	int n;
+	cin >> n;
+	dy[1] = 1;
+	dy[2] = 2;
+
+	for (int i = 3; i <= n; i++) {
+		dy[i] = dy[i - 2] + dy[i - 1];
+	}
+	cout << dy[n] << endl;
+}
+
