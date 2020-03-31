8714323b193ca977706303355074ab25a31e6095
thisisziihee <44192641+thisisziihee@users.noreply.github.com>
2020-03-31 15:15:03 +0900

    DP, LIS 응용

---
+++ 백준/11722_가장 긴 감소하는 부분 수열.cpp
@@ -0,0 +1,34 @@
+/*
+수열 A가 주어졌을 때, 가장 긴 감소하는 부분 수열을 구하는 프로그램을 작성하시오.
+예를 들어, 수열 A = {10, 30, 10, 20, 20, 10} 인 경우에 가장 긴 감소하는 부분 수열은 A = {30, 20, 10}  이고, 길이는 3이다.
+
+입력
+첫째 줄에 수열 A의 크기 N (1 ≤ N ≤ 1,000)이 주어진다.
+둘째 줄에는 수열 A를 이루고 있는 Ai가 주어진다. (1 ≤ Ai ≤ 1,000)
+
+출력
+첫째 줄에 수열 A의 가장 긴 감소하는 부분 수열의 길이를 출력한다.
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	vector<int> arr(n, 0);
+	for (int i = 0; i < n; i++) cin >> arr[i];
+
+
+	vector<int> dp(n, 0);
+	dp[0] = 1;
+	for (int i = 1; i < n; i++) {
+		for (int j = 0; j < i; j++) {
+			if (arr[i] < arr[j]) {
+				dp[i] = max(dp[i], dp[j] + 1);
+			}
+		}
+		if (dp[i] == 0) dp[i] = 1;
+	}
+
+	cout << *max_element(dp.begin(), dp.end()) << endl;
+}
